96017ce6af62816c8907ad3ec0f2fc7536cde26f
mtswip <92600965+mtswip@users.noreply.github.com>
2022-02-15 20:29:17 -0600

    Rewrote to skip checking for unnecessary values. This massively reduced execution time.

--- perfect_nums/src/main.rs
+++ perfect_nums/src/main.rs
@@ -1,9 +1,24 @@
 use std::io; //not sure this is necessary, but I can't tell now cause I'm testing a very large number
+use std::f64;
+use std::collections;
+
+fn check(mut s: u64, n: u64, ip: u64, feedback: bool) -> u64 {
+    if (n % ip) == 0 { //this function should handle divisors higher than sqrt of the number
+        s = s + ip;
+        if feedback {
+            println!("Iteration {}/{}", ip, n);
+        }
+    }
+    return s;
+}
 
 fn main() {
     //large test number will be 2305843008139952128
-    const n: u64 = 100000000; //eventually planning to change to mut u64
+    //another test number is 137438691328
+    //small test numbers are 6, 28, 496, and 8128
+    const n: u64 = 137438691328; //eventually planning to change to mut u64
     let mut s = 0;
+    let mut stack = Vec::new();
     
     //will worry about input later
     //println!("Input the number: ");
@@ -12,23 +27,53 @@ fn main() {
         //.read_line(&mut n)
         //.expect("Failed to read input");
 
-    if (n > 1000000) { //this number should be roughly enough that loading feedback is required during execution
+    let f = n as f64;
+    let _root = f.sqrt();
+    let iterations = _root.round() as u64;
+
+    if n > 1000000 { //this number should be roughly enough that loading feedback is required during execution
         //should display a loading bar
-        for i in 1..n {
+        for i in 1..iterations+1 {
             if (n % i) == 0 { //this checks for all of a number's divisors
                 s = s + i; //this adds the divisor to the total
+                stack.push(i);
                 println!("Iteration {}/{}", i, n); //I know this is a bad way to do this lol
                 //printing within the if statement keeps it from printing way too many times
             }
         }
+
+        while stack.len() > 1 {
+            let enm = stack.pop();
+            let popped = match enm { //match required because pop returns an enum
+                Some(number) => number,
+                None => 0,
+            };
+            let ip = n / popped;
+            s = check(s, n, ip, true);
+        }
+
+        stack.pop(); //to fully empty the stack
     }
 
     else { //this does the for loop without the print statement to prevent clutter for short execution times
-        for i in 1..n {
+        for i in 1..iterations+1 {
             if (n % i) == 0 { //this checks for all of a number's divisors
                 s = s + i; //this adds the divisor to the total
+                stack.push(i); //this pushes to a stack for later
             }
         }
+
+        while stack.len() > 1 {
+            let enm = stack.pop();
+            let popped = match enm {
+                Some(number) => number,
+                None => 0,
+            };
+            let ip = n / popped;
+            s = check(s, n, ip, false);
+        }
+
+        stack.pop();
     }
 
     
---
+++ perfect_nums/target/debug/deps/perfect_nums-3116cad212a6654f.d
@@ -0,0 +1,5 @@
+C:\Users\Sam Horn\Documents\Github\perfect-nums\perfect_nums\target\debug\deps\perfect_nums-3116cad212a6654f.rmeta: src\main.rs
+
+C:\Users\Sam Horn\Documents\Github\perfect-nums\perfect_nums\target\debug\deps\perfect_nums-3116cad212a6654f.d: src\main.rs
+
+src\main.rs:
